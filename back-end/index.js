890daf46ebd0b7c706eb12e6e7c70ca83ee96a9d
anastasiaursu98 <anastasiaursu98@gmail.com>
2026-01-09 15:29:14 +0200

    fixed index.js

--- back-end/index.js
+++ back-end/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import authRoutes from "./src/routes/auth.route.js";
 import messageRoutes from "./src/routes/message.route.js";
 import dotenv from "dotenv";
-import { connectDB } from "./src/lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import { connectDB } from "./src/lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -12,6 +14,13 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        credentials: true
+    }
+));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
