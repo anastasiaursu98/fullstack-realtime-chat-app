fd0b78122c6f0c07c4cb24d8f671edb1fa645aaa
anastasiaursu98 <anastasiaursu98@gmail.com>
2026-01-14 13:48:20 +0200

    vercel file

--- back-end/index.js
+++ back-end/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "https://fullstack-realtime-chat-app-7j7n.vercel.app",
+        origin: process.env.FRONTEND_URL || "http://localhost:3000",
         credentials: true,
     })
 );
