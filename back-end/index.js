5653eb99834281a753a8998aeab2b59a7c506bdf
anastasiaursu98 <anastasiaursu98@gmail.com>
2026-01-16 15:45:19 +0200

    chat updates

--- back-end/index.js
+++ back-end/index.js
@@ -16,14 +16,15 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(
     cors({
         origin: process.env.FRONTEND_URL,
         credentials: true,
     })
 );
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
 
 // Middleware to ensure DB connection
 app.use(async (req, res, next) => {
